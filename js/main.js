271f58503ab682bc2987d01c138895532dd21b67
Andrew Kirilkin <124495133+AndrewBykov@users.noreply.github.com>
2023-10-23 14:46:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,16 +8,15 @@ function checkingCompositeNumber(input) {
     let str = input.value
     // Numbers
     const pattern = '0123456789876543210';
-    // Letters
-    const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    // Letter
     const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
     // Letters to reverse
-    const uppercaseLettersReverse = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').reverse('').join('')
     const lowercaseLettersReverse = 'abcdefghijklmnopqrstuvwxyz'.split('').reverse('').join('')
     // Folding all letters
-    const upperAllLowerAll = uppercaseLetters + uppercaseLettersReverse + lowercaseLetters + lowercaseLettersReverse
+    const upperAllLowerAll = lowercaseLetters + lowercaseLettersReverse
 
     str = str.replace(/[\s\.\-\,]/g, "")
+    str = str.toLowerCase()
 
     if(str === '') {
       return 'У вас пустое поле ввода'
